fffadc447e2f32ddd80a7a5e78f2d17a4ebe5595
Kai Haehnle <kh120@hdm-stuttgart.de>
2022-01-03 16:02:48 +0100

    improved state-map structure

--- src/terminal_app/core.clj
+++ src/terminal_app/core.clj
@@ -9,55 +9,41 @@
 
 (defn prepare-line [files i width]
   (if (< i (count files))
-    (let [line (files i)]
+    (let [line (get-in files [i :name])]
       (str " "
            (subs line 0 (min (count line) width))
            (apply str (repeat (- width (count line)) \space))))
     (apply str (repeat (inc width) \space))))
 
-(defn rerender-sel [state]
-  (s/put-string scr
-                40
-                (state :sel-old)
-                (prepare-line (state :files) (state :sel-old) 40))
-  (s/put-string scr
-                40
-                (state :sel)
-                (prepare-line (state :files) (state :sel) 40)
-                {:fg :black :bg :yellow}))
+(defn get-line-style [dir i]
+  (if (= (+ i (dir :scroll-pos)) (dir :sel)) {:fg :black
+                                              :bg :cyan}
+      (if (get-in dir [:files i :dir?])
+        {:fg :cyan}
+        {})))
 
-(defn render-files [col-start col-width row-start row-height files sel scroll-pos]
-    (doseq [i (range row-height)]
-      (s/put-string scr
-                    col-start
-                    (+ i row-start)
-                    (prepare-line files (+ i scroll-pos) col-width)
-                    (if (= (+ i scroll-pos) sel) {:fg :black
-                                   :bg :cyan} {}))))
+(defn render-files [col-start col-width ly dir]
+  (doseq [i (range (ly :list-height))]
+    (s/put-string scr
+                  col-start
+                  (+ i (ly :top-bar-height))
+                  (prepare-line (dir :files) (+ i (dir :scroll-pos)) col-width)
+                  (get-line-style dir i))))
 
 (defn do-render [state]
   (let [ly (state :layout)]
     (render-files 0
                   (- (ly :col1-char) 2)
-                  (ly :top-bar-height)
-                  (ly :list-height)
-                  (get-in state [:par-dir :files])
-                  (get-in state [:par-dir :sel])
-                  (get-in state [:par-dir :scroll-pos]))
+                  ly
+                  (state :par-dir))
     (render-files (ly :col1-char)
                   (- (ly :col2-char) (ly :col1-char) 2)
-                  (ly :top-bar-height)
-                  (ly :list-height)
-                  (state :files)
-                  (state :sel)
-                  (state :scroll-pos))
+                  ly
+                  (state :dir))
     (render-files (ly :col2-char)
                   (- (get-in ly [:size 0]) (ly :col2-char))
-                  (ly :top-bar-height)
-                  (ly :list-height)
-                  (state :files)
-                  (state :sel)
-                  0)))
+                  ly
+                  (state :dir))))
 
 (defn handle-input [state]
   (case (s/get-key-blocking scr)
--- src/terminal_app/navigate.clj
+++ src/terminal_app/navigate.clj
@@ -1,44 +1,57 @@
 (ns terminal-app.navigate
   (:require [clojure.java.io :as io]))
 
-(defn generate-file-list [file]
+(defn generate-file-list-old [file]
   (vec (sort (mapv str (.list file)))))
+
+(defn generate-file-list [file]
+  (mapv (fn [f] {:obj f
+                 :name (.getName f)
+                 :dir? (.isDirectory f)})
+        (.listFiles file)))
+
+;; (generate-file-list (io/file "/home/kai"))
+
+(defn get-sel-file [dir]
+  (get-in dir [:files (dir :sel) :obj]))
 
 (defn init-state [path]
   (let [file (io/file path)
         par-file (.getParentFile file)
         par-files (generate-file-list par-file)]
-    {:file file
-     :files (generate-file-list file)
-     :sel 0
-     :sel-prev nil
-     :scroll-pos 0
-     :par-dir {:file par-file
-               :files par-files
-               :sel (.indexOf par-files (.getName file))
-               :scroll-pos 0}
+    {:dir      {:file file
+                :files (generate-file-list file)
+                :sel 0
+                :scroll-pos 0}
+     :par-dir  {:file par-file
+                :files par-files
+                :sel (.indexOf par-files (.getName file))
+                :scroll-pos 0}
      :prev-dir {}
-     :layout {:size []
-              :top-bar-height 1
-              :bottom-bar-height 1
-              :col1-percent 0.2
-              :col2-percent 0.6}}))
+     :layout   {:size []
+                :top-bar-height 1
+                :bottom-bar-height 1
+                :col1-percent 0.2
+                :col2-percent 0.6}}))
 
 (defn sel-down [state]
-  (if (< (state :sel) (- (count (state :files)) 1))
+  (if (< (get-in state [:dir :sel])
+         (- (count (get-in state [:dir :files])) 1))
     (as-> state st
-      (update st :sel inc)
-      (if (>= (+ (st :sel) (st :scroll-pos)) (get-in st [:layout :list-height]))
-        (update st :scroll-pos inc)
+      (update-in st [:dir :sel] inc)
+      (if (>= (+ (get-in st [:dir :sel]) (get-in st [:dir :scroll-pos]))
+              (get-in st [:layout :list-height]))
+        (update-in st [:dir :scroll-pos] inc)
         st))
     state))
 
 (defn sel-up [state]
-  (if (> (state :sel) 0)
+  (if (> (get-in state [:dir :sel]) 0)
      (as-> state st
-       (update st :sel dec)
-       (if (< (st :sel) (st :scroll-pos))
-         (update st :scroll-pos dec)
+       (update-in st [:dir :sel] dec)
+       (if (< (get-in st [:dir :sel])
+              (get-in st [:dir :scroll-pos]))
+         (update-in st [:dir :scroll-pos] dec)
          st))
      state))
 
@@ -46,28 +59,20 @@
   (let [name (.getName (get-in state [:par-dir :file]))
         par-file (.getParentFile (get-in state [:par-dir :file]))
         par-files (generate-file-list par-file)
-        par-sel (.indexOf par-files name)]
+        par-sel (.indexOf (mapv (fn [f] (f :name)) par-files) name)]
     (-> state
-        (assoc :file (get-in state [:par-dir :file]))
-        (assoc :files (get-in state [:par-dir :files]))
-        (assoc :sel (get-in state [:par-dir :sel]))
-        (assoc :scroll-pos (get-in state [:par-dir :scroll-pos]))
+        (assoc :dir (state :par-dir))
         (assoc-in [:par-dir :file] par-file)
         (assoc-in [:par-dir :files] par-files)
         (assoc-in [:par-dir :sel] par-sel)
         (assoc-in [:par-dir :scroll-pos] (max 0 (- par-sel (get-in state [:layout :list-height]) -1))))))
 
 (defn folder-down [state]
-  (let [file (io/file (str (.getAbsolutePath (state :file))
-                           "/"
-                           ((state :files) (state :sel))))
+  (let [file (get-sel-file (state :dir))
         files (generate-file-list file)]
     (-> state
-      (assoc-in [:par-dir :file] (state :file))
-      (assoc-in [:par-dir :files] (state :files))
-      (assoc-in [:par-dir :sel] (state :sel))
-      (assoc-in [:par-dir :scroll-pos] (state :scroll-pos))
-      (assoc :file file)
-      (assoc :files files)
-      (assoc :sel 0)
-      (assoc :scroll-pos 0))))
+        (assoc :par-dir (state :dir))
+        (assoc-in [:dir :file] file)
+        (assoc-in [:dir :files] files)
+        (assoc-in [:dir :sel] 0)
+        (assoc-in [:dir :scroll-pos] 0))))
