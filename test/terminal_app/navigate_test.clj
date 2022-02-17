658035aa93302173267fdc458d8fb133d99d8602
Kai Haehnle <kh120@hdm-stuttgart.de>
2022-02-17 16:10:59 +0100

    added firste tests

--- test/terminal_app/navigate_test.clj
+++ test/terminal_app/navigate_test.clj
@@ -10,10 +10,32 @@
 ;;
 (def tmp-dir "/")
 
-(deftest get-preview
+(deftest get-preview-test
   (is (= (with-tmp-dir tmp-dir
            ;; (.mkdir (io/file tmp-dir "folder"))
            (spit (io/file tmp-dir "foo.txt") "I'm here")
-           (get-preview! (generate-file-list! (io/file tmp-dir))))
-         [{:file nil, :filess nil, :sel nil, :scroll-pos nil, :content ["I'm here"]}])
+           (get-preview! (generate-file-list! tmp-dir)))
+         [{:path nil, :filess nil, :sel nil, :scroll-pos nil, :content ["I'm here"]}])
       "gets a 'files'-vector and returns a vector with directory information or file information/content for every file in it"))
+
+(deftest sort-files-test
+  (is (= (sort-files [{:name "b" :nodir? nil :hidden? nil}
+                      {:name "a" :nodir? true :hidden? nil}
+                      {:name "c" :nodir? true :hidden? true}
+                      {:name "a" :nodir? nil :hidden? true}])
+         [{:name "b" :nodir? nil :hidden? nil}
+          {:name "a" :nodir? nil :hidden? true}
+          {:name "a" :nodir? true :hidden? nil}
+          {:name "c" :nodir? true :hidden? true}])))
+
+(deftest bytes-to-n-test
+  (is (= (bytes-to-n 10000 1)
+         "9.765"))
+  (is (= (bytes-to-n 1000000 2)
+         "0.953")))
+
+(deftest get-file-size-str-test
+  (is (= (with-tmp-dir tmp-dir
+           (spit (io/file tmp-dir "foo.txt") "I'm here")
+           (get-file-size-str (io/file tmp-dir "foo.txt")))
+         "8 B")))
